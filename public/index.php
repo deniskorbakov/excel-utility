f726ad3e23effc2714bb129e1d2dfc65ccb3a718
deniskorbakov <korbakovd@gmail.com>
2023-09-26 21:22:17 +0300

    stable version

--- public/index.php
+++ public/index.php
@@ -7,12 +7,23 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        input {
+            height: 30px;
+            padding: 5px;
+        }
+
+        body {
+            background: #222222;
+        }
+    </style>
 </head>
 <body>
 <center>
     <form action="" method="POST">
         <div>
             <button class="add-button" style="padding: 10px; font-size: 20px; color: #ffffff; background-color: #80109f; border: none;" type="button">Добавить еще поля</button>
+            <button class="delete-inputs" type="button" style="padding: 10px; font-size: 20px; color: #ffffff; background-color: #9f1031; border: none;">Удалить последние поля</button>
         </div>
 
         <div style="margin-top: 30px">
@@ -24,8 +35,8 @@
             <input type="text" name="pathImg[]" placeholder="Введите путь для замены">
         </div>
 
-        <div style="margin-top: 50px;">
-            <button style="padding: 10px; font-size: 20px; color: #ffffff; background-color: #11b814; border: none;" type="submit">Добавить поля в таблицу</button>
+        <div style="margin-top: 25px;">
+            <button style="padding: 10px; font-size: 20px; color: #ffffff; background-color: #11b814; border: none;" type="submit">Изменить картинки в таблице</button>
 
             <select name="selectSeparator" style="padding: 10px; font-size: 20px; color: #ffffff; background-color: rgba(0,0,0,0.5); border: none;">
                 <option value=";">Разделить знаком - ( ; )</option>
@@ -39,13 +50,20 @@
 <script>
     let inputWrapper = document.querySelector('.input-wrapper');
     let addButton = document.querySelector('.add-button');
+    let deleteInputs = document.querySelector('.delete-inputs');
 
     addButton.addEventListener('click', function() {
         let newInput = document.createElement('div');
-        newInput.innerHTML = '<br> <input type="text" name="formatName[]" placeholder="Введите формат товара"> <input type="text" name="pathImg[]" placeholder="Введите путь для замены">';
+        newInput.className = 'inputs';
+        newInput.innerHTML = '<br> <input class="newFormatName" type="text" name="formatName[]" placeholder="Введите формат товара"> <input class="newPathImg" type="text" name="pathImg[]" placeholder="Введите путь для замены">';
         inputWrapper.appendChild(newInput);
     });
 
+    deleteInputs.addEventListener('click', function() {
+        let newInputs = document.querySelector('.inputs');
+
+        newInputs.remove();
+    });
 </script>
 </body>
 </html>
@@ -53,7 +71,11 @@
 <?php
 
 if (isset($_POST['productName'], $_POST['formatName'], $_POST['pathImg'], $_POST['selectSeparator'])) {
-    // Открытие файла с данными
+//очищаем массивы от пустых значений
+    $formatName = array_diff($_POST['formatName'], array(""));
+    $pathImg = array_diff($_POST['pathImg'], array(""));
+
+// Открытие файла с данными
     $file = fopen('../tables/lioni.csv', 'r+');
 
 // Чтение заголовков столбцов
@@ -79,11 +101,11 @@ if (isset($_POST['productName'], $_POST['formatName'], $_POST['pathImg'], $_POST
 // Изменение значений в столбцах preview_picture и detail_picture
     $count = 0;
 
-    foreach ($_POST['formatName'] as $key => $format) {
+    foreach ($formatName as $key => $format) {
         foreach ($tableData as &$row) {
             if ($row[array_search('IE_NAME', $headers)] == $_POST['productName'] && $row[array_search('IP_PROP1295', $headers)] ==  $format ) {
-                $row[$preview_picture_index] = $_POST['pathImg'][$key];
-                $row[$detail_picture_index] = $_POST['pathImg'][$key];
+                $row[$preview_picture_index] = $pathImg[$key];
+                $row[$detail_picture_index] = $pathImg[$key];
 
                 $count++;
             }
@@ -91,12 +113,12 @@ if (isset($_POST['productName'], $_POST['formatName'], $_POST['pathImg'], $_POST
     }
 
     if ($count == 0) {
-        echo '<center style="margin-top: 30px;"> <h3>Не найдены товары</h3> </center>';
+        echo '<center style="margin-top: 30px; color: white;"> <h3>Не найдены товары</h3> </center>';
         exit();
     }
 
 // Открытие файла для записи измененных данных
-    $file = fopen('../tables/output.csv', 'r+');
+    $file = fopen('../tables/output.csv', 'w');
 
 // Запись заголовков столбцов
     fputcsv($file, $headers, $_POST['selectSeparator']);
@@ -109,10 +131,10 @@ if (isset($_POST['productName'], $_POST['formatName'], $_POST['pathImg'], $_POST
 // Закрытие файла
     fclose($file);
 
-    echo '<center style="margin-top: 30px;"> <h3>кол-во измененных записей</h3> - ' . $count . '</center>' ;
+    echo '<center style="margin-top: 30px; color: white;"> <h3>кол-во измененных записей</h3>' . $count . '</center>' ;
 
 } else {
-   echo '<center style="margin-top: 30px;"> <h3>Вставьте все данные</h3> </center>';
+   echo '<center style="margin-top: 30px; color: white;"> <h3>Вставьте все данные</h3> </center>';
 }
 
 
